1a40c83d4607762488af9fb11353255974000949
Junbang Huang <wongzeonbong@gmail.com>
2016-05-24 17:05:50 +0800

    finish Request Download connection without timer

--- src/akh_client_main.c
+++ src/akh_client_main.c
@@ -9,42 +9,59 @@
 #include "message_util.h"
 #include "error_handling.h"
 
+#define MAX_BUFFER_SIZE 128 * 1024 // maximum buffer size 128 kb
 int main(int argc, char *argv[])
 {
     if(argc != 3) {
         printf("Usage: %s <IP> <port>\n", argv[0]);
         exit(1);
     }
-
-    akh_pdu_header header = createHeader(RD, 0);
-    akh_pdu_body body = "Hello World";
-    packet pac;
-    size_t pac_len = createPacket(&pac, header, body, strlen(body));
-    /* printf("pac_len => %d\n", pac_len); */
-    /* displayHeader(*(akh_pdu_header *)pac); */
-    /* printf("body => %s\n", pac + sizeof(header)); */
-
-
+    
     int sock;
     socklen_t adr_sz;
     struct sockaddr_in serv_adr, from_adr;
-    char message[20] = "hello world!!!";
-    int str_len;
-
-    sock = socket(PF_INET, SOCK_DGRAM, 0);
-    if(sock == -1)
-        error_handling("socket() error");
-
     memset(&serv_adr, 0, sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
     serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_adr.sin_port = htons(atoi(argv[2]));
 
 
+    sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if(sock == -1) {
+    	error_handling("create socket error");
+    }
+
+    // request download
+    akh_pdu_header header = createHeader(RD, 0);
+    akh_pdu_body body = "test.txt";
+    packet pac;
+    size_t pac_len = createPacket(&pac, header, body, strlen(body));
+
+    char response[20];
+    int str_len;
+
+    // send RD to server
     sendto(sock, pac, pac_len, 0, (struct sockaddr *)&serv_adr, sizeof(serv_adr));
-    str_len = recvfrom(sock, message, 20, 0, (struct sockaddr *)&from_adr, &adr_sz);
-    message[str_len] = 0;
-    printf("Message from server: %s\n", message);
+    str_len = recvfrom(sock, response, 20, 0, (struct sockaddr *)&from_adr, &adr_sz);
+    response[str_len] = 0;
+
+    // get file size
+    char fsize[4];
+    int file_size;
+    memcpy(fsize, response + 12, 4);
+    file_size = atoi(fsize);
+    printf("Message from server: %s\n", response);
+    printf("File size: %d\n", file_size);
+
+    // set up buffer
+    char buffer[MAX_BUFFER_SIZE];
+    memset(&buffer, 0, sizeof(char) * MAX_BUFFER_SIZE);
+
+    // send RS to request file segment
+    uint32_t seq_num = 12345;
+    header = createHeader(RS, seq_num);
+    body = "1024"; // 1 kb
+    pac_len = createPacket(&pac, header, body, strlen(body));
 
     close(sock);
     deletePacket(pac);
