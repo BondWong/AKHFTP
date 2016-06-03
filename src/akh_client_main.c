8f254fa78d0494a0de64acf0af9af334a9d7d4e0
sibyl1956 <sibyl1956@gmail.com>
2016-06-03 17:07:41 -0400

    Add a small command line UI

--- src/akh_client_main.c
+++ src/akh_client_main.c
@@ -20,14 +20,34 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc != 5) {
+    if(argc != 5 && argc != 1) {
         printf("Usage: %s <IP> <port> <option> <filename>\n", argv[0]);
         printf("<option>\n");
         printf("-d <filename>: download file\n");
         printf("-u <filename>: upload file\n");
     }
-
-    char *filename = argv[4];
+    // create a command line UI
+    char host_ip[16];
+    char host_port[6];
+    char option[3];
+    char filename[MAX_FILENAME_SIZE];
+    if (argc == 1){
+	printf("Please input IP address of the server\n");
+	scanf("%s", host_ip);
+	printf("Please input port number of the service (default 9190)\n");
+	scanf("%s", host_port);
+	printf("Please input client options '-d' or '-u'\n");
+	scanf("%s", option);
+	printf("Please input file name (e.g. Download: 'server.txt' or Upload: 'client.txt)\n");
+	scanf("%s", filename);
+    }
+    // $make run_client_download or $make run_client_upload
+    if (argc == 5){
+	strcpy(host_ip, argv[1]);
+	strcpy(host_port, argv[2]);
+	strcpy(option, argv[3]);
+	strcpy(filename, argv[4]);
+    }
     int sock;
     struct sockaddr_in serv_adr;
     socklen_t serv_adr_sz;
@@ -36,8 +56,8 @@ int main(int argc, char *argv[])
     memset(&serv_adr, 0, sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
     /**************** CLIENT *******************/
-    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_adr.sin_port = htons(atoi(argv[2]));
+    serv_adr.sin_addr.s_addr = inet_addr(host_ip);
+    serv_adr.sin_port = htons(atoi(host_port));
 
     sock = socket(PF_INET, SOCK_DGRAM, 0); // create UDP socket with queue size 0
     if(sock == -1) {
@@ -49,7 +69,7 @@ int main(int argc, char *argv[])
     getsockname(sock, &created_socket_addr, &created_socket_len);
     char* sa_data = created_socket_addr.sa_data;
 
-    if(strcmp(argv[3], "-d") == 0) {
+    if(strcmp(option, "-d") == 0) {
         if( (filesize = connection_download_client(sock, &serv_adr, filename)) == 0) {
     	    error_handling("sender does not have the file");
         }
@@ -70,7 +90,7 @@ int main(int argc, char *argv[])
             error_handling("connection error");
     }
 
-    else if(strcmp(argv[3], "-u") == 0) {
+    else if(strcmp(option, "-u") == 0) {
         if(connection_upload_client(sock, &serv_adr, filename, &filesize) != 0)
             error_handling("rejected by receiver");
 
